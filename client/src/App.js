37ae58aff6e6d135bcb4c4867cc50738254d6221
Rohan Mallesh <rohanmrb@gmail.com>
2020-12-11 01:21:44 +0530

    Final

--- client/src/App.js
+++ client/src/App.js
@@ -29,11 +29,11 @@ const App = () => {
         <Container>
           <Map setCurrentId={setCurrentId} />
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid item xs={12} sm={4}>
+              <Form currentId={currentId} setCurrentId={setCurrentId} />
+            </Grid>
             <Grid item xs={12} sm={7}>
               <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
             </Grid>
           </Grid>
         </Container>
--- client/src/styles.js
+++ client/src/styles.js
@@ -12,7 +12,7 @@ export default makeStyles(() => ({
   },
   heading: {
     backgroundColor: 'transparent',
-    fontFamily: 'Sans',
+    fontFamily: 'Sans-serif',
     color: 'darkblue',
   },
   image: {
